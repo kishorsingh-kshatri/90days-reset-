81be59fb1161aef365ae26cc93504ad0999deb00
Kshatri <ksingh7@volvocars.com>
2026-05-18 13:54:14 +0200

    Add PWA/iOS support to health tracker

---
+++ health-90-day-tracker/service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "health-90-day-tracker-pwa-v1";
+const ASSETS_TO_CACHE = [
+  ".",
+  "index.html",
+  "styles.css",
+  "app.js",
+  "manifest.json",
+  "icons/icon.svg",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) {
+        return cached;
+      }
+      return fetch(event.request).catch(() => caches.match("index.html"));
+    })
+  );
+});
